3103d277ba066bfd844a6900e9ad3a4006817732
DESKTOP-ERRHRDV\Saloberg <nailik.nomolas@web.de>
2021-05-24 14:00:00 +0200

    building roc is possible

--- every_step_together.py
+++ every_step_together.py
@@ -2,10 +2,12 @@ import lightgbm as lgb
 import numpy as np
 # data: https://github.com/nshomron/covidpred/tree/master/data
 import pandas as pd
+from matplotlib import pyplot as plt
 
 #laad train and validation data
 train_pd=pd.read_csv('data/preprocessed_train.csv', sep=',')
 valid_pd=pd.read_csv('data/preprocessed_valid.csv', sep=',')
+test_pd=pd.read_csv('data/preprocessed_test.csv', sep=',')
 
 #create datasets from lgb
 param_readin = {'feature_pre_filter': False}
@@ -22,21 +24,37 @@ train_param = {'num_leaves': 20, 'objective': 'binary',
 'learning_rate': 0.05,
 'verbose': 1}
 #num_boost_round=603
-bst = lgb.train(train_param, train_data, num_boost_round=10, early_stopping_rounds=5, valid_sets=[validation_data])
-
-#data eval
-data_eval = bst.eval(train_data, name="training")
-print(data_eval)
+bst = lgb.train(train_param, train_data, num_boost_round=80, early_stopping_rounds=5, valid_sets=[validation_data])
 
 #predict
-ypred = bst.predict(train_pd)
-print(type(ypred))
-print(ypred)
+ypred = bst.predict(test_pd)
 mask = ypred > 0.5
-print(mask)
-train_pd["predict"] = mask
-train_pd.to_csv('data/predicted_train_17Uhr.csv', index=False)
+#validation/testset
+vy = test_pd["corona_result"]
+vX = test_pd.drop(labels="corona_result", axis=1)
+#confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(vy, mask)
+print('\nConfusion matrix\n\n', cm)
+print('\nTrue Positives(TP) = ', cm[0,0])
+print('\nTrue Negatives(TN) = ', cm[1,1])
+print('\nFalse Positives(FP) = ', cm[0,1])
+print('\nFalse Negatives(FN) = ', cm[1,0])
 
-#predict eval
-eval_train = bst.eval_train()
-print(eval_train)
+#roc curve:
+import numpy as np
+from sklearn import metrics
+fpr, tpr, thresholds = metrics.roc_curve(vy, mask, pos_label=1)
+
+plt.figure()
+lw = 2
+plt.plot(fpr, tpr, color='darkorange',
+         lw=lw, label='ROC curve')
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic example')
+plt.legend(loc="lower right")
+plt.show()
--- preprocessing.py
+++ preprocessing.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 #reading in
 
-df=pd.read_csv('data/corona_tested_individuals_ver_0083.english.csv', sep=',')
+df=pd.read_csv('data/corona_tested_individuals_ver_006.english.csv', sep=',')
 # show nan: #11
 df1 = df[df.isna().any(axis=1)]
 print("All nan values:\n", df1)
@@ -26,7 +26,10 @@ df.loc[(df.corona_result == 'negative'),'corona_result']=False
 #how many others? (9 in 2000 rows)
 print("All values of corona result:\n", df.groupby('corona_result').count())
 
-
+print("All values of cough:\n", df.groupby('cough').count())
+print("All values of fever:\n", df.groupby('fever').count())
+df = df[df['fever'] != "None"]
+df = df[df['cough'] != "None"]
 # removing others
 df = df[df['corona_result'] != "other"]
 
@@ -40,17 +43,20 @@ df.loc[(df.age_60_and_above == 'Yes'),'age_60_and_above']=True
 df.loc[(df.age_60_and_above == 'No'),'age_60_and_above']=False
 print("All values of age_60_and_above:\n", df.groupby('age_60_and_above').count())
 
+df = df[df['age_60_and_above'] != "None"] #added for test
 # print('age filtered\n', df)
 
 #gender filtering
 df.loc[(df.gender == 'female'),'gender']=True
 df.loc[(df.gender == 'male'),'gender']=False
+
+df = df[df['gender'] != "None"] #added for test
 # print("gender filtered:\n", df)
 
 #test_indication filtering
-df.loc[(df.test_indication == 'Other'),'test_indication']=0
+df.loc[(df.test_indication == 'Other'),'test_indication']=2
 df.loc[(df.test_indication == 'Contact with confirmed'),'test_indication']=1
-df.loc[(df.test_indication == 'Abroad'),'test_indication']=2
+df.loc[(df.test_indication == 'Abroad'),'test_indication']=0
 # print(df.test_indication.unique())
 
 print("finished filtering\n", df)
@@ -60,44 +66,46 @@ print(df.dtypes)
 
 #spliting into test, train and validation data
 # 4 - 1: train valid
-mask = np.random.rand(len(df)) < 0.8
-print(mask)
-train = df[mask]
-
-validation = df[~mask]
-
-print("length of train: ", len(train), "\nlength of valid: ", len(validation))
-
-param = {'feature_pre_filter': False}
+### comment
+### mask = np.random.rand(len(df)) < 0.8
+### print(mask)
+### train = df[mask]
+###
+### validation = df[~mask]
+###
+### print("length of train: ", len(train), "\nlength of valid: ", len(validation))
+###
+### param = {'feature_pre_filter': False}
 
 # df.to_csv('data/preprocessed_full.csv', index=False)
-train.to_csv('data/preprocessed_train.csv', index=False)
-validation_data.to_csv('data/preprocessed_valid.csv', index=False)
+df.to_csv('data/preprocessed_test.csv', index=False)
+### train.to_csv('data/preprocessed_train.csv', index=False)
+### validation_data.to_csv('data/preprocessed_valid.csv', index=False)
 #end
 
 #tried to compute everthing in one file:
 
-
-train_data = lgb.Dataset(train, params=param, label=train['corona_result']).construct()
-
-validation_data = lgb.Dataset(validation, params=param,  label=validation['corona_result'], reference=train_data).construct()
-
-
-param = {'num_leaves': 20, 'objective': 'binary',
-'min_data_in_leaf': 4,
-'feature_fraction': 0.2,
-'bagging_fraction': 0.8,
-'bagging_freq': 5,
-'learning_rate': 0.05,
-'verbose': 1}
-
-bst = lgb.train(param, train_data, num_boost_round=603, early_stopping_rounds=5, valid_sets=[validation_data])
-
-
-
-bst.save_model('model_round-603_test.txt') #save model:
-
-asdf = bst.eval(validation_data, name="training")
-
-
-print("Rows of validation: ",validation_data.num_data(), "\nColumns of validation: ", validation_data.num_feature())
+#comment
+# train_data = lgb.Dataset(train, params=param, label=train['corona_result']).construct()
+#
+# validation_data = lgb.Dataset(validation, params=param,  label=validation['corona_result'], reference=train_data).construct()
+#
+#
+# param = {'num_leaves': 20, 'objective': 'binary',
+# 'min_data_in_leaf': 4,
+# 'feature_fraction': 0.2,
+# 'bagging_fraction': 0.8,
+# 'bagging_freq': 5,
+# 'learning_rate': 0.05,
+# 'verbose': 1}
+#
+# bst = lgb.train(param, train_data, num_boost_round=603, early_stopping_rounds=5, valid_sets=[validation_data])
+#
+#
+#
+# bst.save_model('model_round-603_test.txt') #save model:
+#
+# asdf = bst.eval(validation_data, name="training")
+#
+#
+# print("Rows of validation: ",validation_data.num_data(), "\nColumns of validation: ", validation_data.num_feature())
